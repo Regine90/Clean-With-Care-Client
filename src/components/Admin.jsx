2f919dbb3ddc2a813acba4e1180dca2d8368ff3e
Regine Valmont <sarahsmithregine90@gmail.com>
2025-06-23 20:25:48 -0400

    Updating

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,4 +1,4 @@
-// useEffect(() => {
-//     fetch("http://localhost:3000/api/people", {method: "Get"} )
-//     .then((response) => )
-// })
+useEffect(() => {
+    fetch("http://localhost:3000/api/people", {method: "Get"} )
+    .then((response) => )
+})
